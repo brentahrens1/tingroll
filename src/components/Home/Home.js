3817d201c4b8fedca860fc9fe36349131156ad13
Brent Ahrens <cidida@gmail.com>
2020-09-11 19:32:28 -0700

    adds hover effect to projects

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,11 +9,16 @@ const Home = () => {
         {
         projects.map(project => 
             <div className="home__content">
-                <div className="home__content-box">
-                    <p className="home__content-category">{project.category}</p>
-                    <h1 className="home__content-header">{project.header}</h1> 
-                </div>
-                <p className="home__content-decscription">{project.description}</p>
+                <a href="#">
+                    <div className="home__content-box">
+                        <p className="home__content-category">{project.category}</p>
+                        <h1 className="home__content-header header">{project.header}</h1> 
+                    </div>
+                    <div className="home__content-box-2">
+                        <h1 className="home__content-header date">{project.date}</h1>
+                        <p className="home__content-decscription">{project.description}</p>
+                    </div>
+                </a>
             </div>
        )
         }
--- src/const/projects.js
+++ src/const/projects.js
@@ -1,7 +1,20 @@
 export const projects = [
     {
         category: 'Currated Exhibtion & Publication',
-        header: 'Love Me / Love Me Not Feb. 2017 - 2020',
+        header: 'Love Me / Love Me Not',
+        date: 'Feb. 2017 - 2020',
+        description: 'A yearly Valentine’s Day zine co-edited, compiled and curated by me and my best friend Nikita Lamba, featuring work entirely by queer, trans, non-binary, and women artists of color.'
+    },
+    {
+        category: 'Currated Exhibtion & Publication',
+        header: 'Love Me / Love Me Not',
+        date: 'Feb. 2017 - 2020',
+        description: 'A yearly Valentine’s Day zine co-edited, compiled and curated by me and my best friend Nikita Lamba, featuring work entirely by queer, trans, non-binary, and women artists of color.'
+    },
+    {
+        category: 'Currated Exhibtion & Publication',
+        header: 'Love Me / Love Me Not',
+        date: 'Feb. 2017 - 2020',
         description: 'A yearly Valentine’s Day zine co-edited, compiled and curated by me and my best friend Nikita Lamba, featuring work entirely by queer, trans, non-binary, and women artists of color.'
     }
 ]
