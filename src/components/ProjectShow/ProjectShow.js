ee3e99d8d75cf3c6d7e6c1f12942e33f89f1de90
Brent Ahrens <cidida@gmail.com>
2020-09-13 14:18:15 -0700

    adds styling for the show page headers

--- src/components/ProjectShow/ProjectShow.js
+++ src/components/ProjectShow/ProjectShow.js
@@ -15,15 +15,19 @@ const ProjectShow = ({ match }) => {
     {match.isExact && <>
         <div className='project-show'>
             <Link className="project-show__back" to="/"><p>Back</p></Link>
-            <div className="home__content">
-                <div className="home__content-box">
-                    <p className="home__content-category">{project.category}</p>
-                    <h1 className="home__content-header header">{project.header}</h1> 
-                </div>
-                <div className="home__content-box-2">
-                    <h1 className="home__content-header date">{project.date}</h1>
-                    <p className="home__content-decscription">{project.description}</p>
-                </div>
+            <div className="home">
+              <div className="home__content">
+                  <div className="home__content-box">
+                      <p className="home__content-category">{project.category}</p>
+                      <h1 className="home__content-header header">{project.header}</h1> 
+                  </div>
+                  <div className="home__content-box-2">
+                      <div className="floating-div">
+                        <h1 className="home__content-header date">{project.date}</h1>
+                      </div>
+                      <p className="home__content-decscription">{project.description}</p>
+                  </div>
+              </div>
             </div>
         </div>
     </>}
