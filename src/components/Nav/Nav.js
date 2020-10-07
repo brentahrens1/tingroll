c6278fb2a9e8d5a46439b3b4fcff6ae07da6ef3c
Brent Ahrens <cidida@gmail.com>
2020-10-06 20:38:08 -0700

    works on mobile nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -57,12 +57,34 @@ const Nav = () => {
             </div>
             <div className="nav__mobile">
                 <h2 className="nav__header-mobile">Jenevieve Ting</h2>
-                <div className="nav__target" onClick={() => setIsOpen(!isOpen)} />
+                <div className={`${isOpen ? "nav__target-gradient" : "nav__target"}`} onClick={() => setIsOpen(!isOpen)} />
             </div>
             <div className={`overlay ${isOpen ? 'active' : ''}`}>
-                <div className="overlay__links">
-                    <h2>Projects</h2>
-                    <h2>About</h2>
+                <div className="nav__top">
+                    <h2 onClick={() => setClickedAbout(false)} className="nav__header"><Link to="/">Jenevieve Ting</Link></h2>
+                    <div className="nav__list">
+                        <p onClick={projectClicked} className={clickedProjects ? 'underline' : ''}><Link to="/">Projects</Link></p>,
+                        <p onClick={aboutClicked} className={clickedAbout ? 'underline' : ''}><Link to="/about">About</Link></p>
+                    </div>
+                    <div className="nav__location">
+                        <p>Los Angeles, CA</p>
+                        <p>7:03PM 80 Degrees</p>
+                    </div>
+                </div>
+                <div className="nav__bottom">
+                    <div className="nav__contact">
+                        <p>Telephone</p>
+                        <p className="phone"><a href="tel:925-915-0977" target="_blank" rel="noopener noreferrer">+1 925 915 0977</a></p>
+                    </div>
+                    <div className="nav__contact">
+                        <p>Email</p>
+                        <p className="email"><a href="mailto:hello@tingroll.com" target="_blank" rel="noopener noreferrer">hello@tingroll.com</a></p>
+                    </div>
+                    <div className="nav__contact">
+                        <p>Socials</p>
+                        <p className="instagram"><a href="https://www.instagram.com/tingrolls/" target="_blank" rel="noopener noreferrer">@tingrolls</a></p>
+                        <p className="linkedin"><a href="https://www.linkedin.com/in/jenevieveting/" target="_blank" rel="noopener noreferrer">Linkedin</a></p>
+                    </div>
                 </div>
             </div>
         </div>
