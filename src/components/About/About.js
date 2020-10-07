63b760c6d5e9efd4a5e5b5247a1588d924b86913
Brent Ahrens <brentahrens@Brents-MacBook-Pro.local>
2020-10-06 22:35:40 -0700

    adds about desing and styling plus mobile styles

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../../sass/_about.scss'
 
-import profile from '../../assets/images/test.png'
+import profile from '../../assets/images/About/profile.PNG'
 
 const About = () => {
     return (
@@ -16,12 +16,33 @@ const About = () => {
                       <div className="floating-div">
                         <h1 className="home__content-header date">2015-2020</h1>
                       </div>
-                        <p className="home__content-decscription">I interviewed the feminist lightning rod for Ace Reader — a new online publication for Ace Hotel — about love, hope, fear, friendship, growing in concentric circles, how “being grateful is a burden in itself,” and her lifelong commitment to social justice.</p>
+                        <p className="home__content-decscription">Jenevieve Ting (they/them) is a queer, non-binary writer, artist, facilitator, and thinker. They are the co-editor and co-founder of Loves Me | Loves Me Not, a yearly Valentine’s Day zine featuring work entirely by queer, trans, non-binary, and women artists of color. Their work explores feelings of belonging, possibility, gender, hope, and how queerness & gender non-conformity can act as guides or maps for understanding spaciousness both within & outside of our bodies. Their work has been featured in Netflix, BuzzFeed, ATTN:, Banana Magazine, Slant'd, and NAVEL to name a few. At their yolky core, they believe that we are all fundamentally deserving of abundance, love, complex futures, transformation, non-binary curiosity, pleasure, fresh lychee, and gender euphoria.</p>
                   </div>
                 </div>
             </div>
             <div className="about__image">
                 <img src={profile} alt="Jenevieve Ting" />
+            </div>
+            <div className="about__bottom-content">
+              <h1>Say hi if you like ♡ (✿ ͡◕ ᴗ◕)つ━━✫・*。</h1>
+              <div className="about-credits">
+                <div className="about-credits__section">
+                  <p>Photography</p>
+                  <p>JT</p>
+                </div>
+                <div className="about-credits__section">
+                  <p>Website desinged by</p>
+                  <p>Karlo Muro</p>
+                </div>
+                <div className="about-credits__section">
+                  <p>Developed by</p>
+                  <p>Brent Ahrens</p>
+                </div>
+                <div className="about-credits__section">
+                  <p>Resume</p>
+                  <p>CV</p>
+                </div>
+              </div>
             </div>
         </div>
     )
